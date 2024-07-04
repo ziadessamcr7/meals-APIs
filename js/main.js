c51cb8677d4c003dbc1725e5629532bb68fb260a
ziad elsharkawy <ziadessamcr7@gmail.com>
2024-07-04 12:40:50 +0300

    adding certificate js file

--- js/main.js
+++ js/main.js
@@ -3,23 +3,39 @@ var myMealsHttpReq = new XMLHttpRequest()
 var mealsArray = []
 
 
-myMealsHttpReq.open('GET' , `https://www.themealdb.com/api/json/v1/1/search.php?s=`)
+myMealsHttpReq.open('GET', `https://www.themealdb.com/api/json/v1/1/search.php?s=`)
 
 myMealsHttpReq.send()
 
-myMealsHttpReq.addEventListener('loadend' , function(){
-    if( myMealsHttpReq.status == 200 ){
-        mealsArray = JSON.parse(  myMealsHttpReq.response ).meals
-        console.log( mealsArray )
+myMealsHttpReq.addEventListener('loadend', function () {
+    if (myMealsHttpReq.status == 200) {
+        mealsArray = JSON.parse(myMealsHttpReq.response).meals
+        console.log(mealsArray)
         displayMeals()
     }
 })
 
 
+
+
 function displayMeals() {
+
+    document.querySelector('.allCountries').classList.add('d-none')
+    document.querySelector('.allIngredients').classList.add('d-none')
+    document.querySelector('.contactUs').classList.add('d-none')
+    document.querySelector('#info').classList.add('d-none')
+    document.querySelector('.mealsCaregory').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
+    document.querySelector('.allCategory').classList.add('d-none')
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
+
+
+    document.querySelector('.allMeals').classList.remove('d-none')
+
+
     var cartona = ``;
 
-    for( var i = 0 ; i < mealsArray.length; i++  ) {
+    for (var i = 0; i < mealsArray.length; i++) {
 
         cartona += `    <div class="col-lg-3"> 
         <article  onclick="displayMealInfo(${i})">
@@ -32,37 +48,48 @@ function displayMeals() {
     document.querySelector('.allMeals').innerHTML = cartona
 }
 
-$('.fa-bars').click( function(){
-    $('#side-nav').animate({left: "0px"}, 600)
+$('.fa-bars').click(function () {
+    $('#side-nav').animate({ left: "0px" }, 600)
     $('.fa-x').css('display', 'block')
     $('.fa-bars').css('display', 'none')
     $('.inner-menu').css('display', 'flex')
 
+})
 
-} )
-
-$('.fa-x').click( function(){
-    $('#side-nav').animate({left: "-280px"}, 600)
+$('.fa-x').click(function () {
+    $('#side-nav').animate({ left: "-280px" }, 600)
     $('.fa-x').css('display', 'none')
     $('.fa-bars').css('display', 'block')
 
-} )
+})
 
 
-$(window).on('load', function(){
+
+
+$(window).on('load', function () {
     $('#spinner').fadeOut('1000')
-} )
+})
 
 
 let search = document.querySelector('#search')
 
-search.addEventListener('click', function(){
+search.addEventListener('click', function () {
     displaySearchInputs()
-} )
+})
 
-function displaySearchInputs(){
+function displaySearchInputs() {
+
+    document.querySelector('.contactUs').classList.add('d-none')
+    document.querySelector('.allCategory').classList.add('d-none')
+    document.querySelector('.allCountries').classList.add('d-none')
+    document.querySelector('.allIngredients').classList.add('d-none')
+    document.querySelector('#info').classList.add('d-none')
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
+
+    document.querySelector('.allMeals').classList.remove('d-none')
     document.querySelector('.search-inputs').classList.remove('d-none')
-    $('#side-nav').animate({left: "-280px"}, 800)
+
+    $('#side-nav').animate({ left: "-280px" }, 800)
     $('.fa-x').css('display', 'none')
     $('.fa-bars').css('display', 'block')
 
@@ -71,34 +98,34 @@ function displaySearchInputs(){
 }
 
 
-function getCategory(){
+function getCategory() {
 
     let myCategoryHttpReq = new XMLHttpRequest()
 
-    myCategoryHttpReq.open('GET','https://www.themealdb.com/api/json/v1/1/categories.php')
+    myCategoryHttpReq.open('GET', 'https://www.themealdb.com/api/json/v1/1/categories.php')
 
     myCategoryHttpReq.send()
 
     categoryArray = []
 
-    myCategoryHttpReq.addEventListener('loadend', function(){
-        if(myCategoryHttpReq.status == 200){
-            categoryArray = JSON.parse( myCategoryHttpReq.response ).categories
+    myCategoryHttpReq.addEventListener('loadend', function () {
+        if (myCategoryHttpReq.status == 200) {
+            categoryArray = JSON.parse(myCategoryHttpReq.response).categories
             displayCategory()
         }
     })
 }
 
-function displayCategory(){
+function displayCategory() {
     document.querySelector('.allMeals').classList.add('d-none')
     document.querySelector('.allCountries').classList.add('d-none')
     document.querySelector('.allIngredients').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('#info').classList.add('d-none')
     document.querySelector('.mealsCaregory').classList.add('d-none')
-    document.querySelector('.search-inputs').classList.add('d-none')
-    document.querySelector('.contactUs').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
     document.querySelector('.allCategory').classList.add('d-none')
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
 
 
 
@@ -106,7 +133,7 @@ function displayCategory(){
 
     var cartona = ``
 
-    for( var i = 0; i < categoryArray.length; i++  ){
+    for (var i = 0; i < categoryArray.length; i++) {
         cartona += `    <div class="col-lg-3"> 
         <article onclick="getMealsByCategory('${categoryArray[i].strCategory}')" >
             <img src="${categoryArray[i].strCategoryThumb}" alt="kb9" class="w-100 rounded-3">
@@ -121,41 +148,42 @@ function displayCategory(){
 
     document.querySelector('.allCategory').innerHTML = cartona
 
-    $('#side-nav').animate({left: "-280px"}, 800)
+    $('#side-nav').animate({ left: "-280px" }, 800)
     $('.fa-x').css('display', 'none')
     $('.fa-bars').css('display', 'block')
 
-    
-    
+
+
 }
 
-function getArea(){
+function getArea() {
 
     let myAreaHttpReq = new XMLHttpRequest()
 
-    myAreaHttpReq.open('GET','https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+    myAreaHttpReq.open('GET', 'https://www.themealdb.com/api/json/v1/1/list.php?a=list')
 
     myAreaHttpReq.send()
 
     areaArray = []
 
-    myAreaHttpReq.addEventListener('loadend', function(){
-        if(myAreaHttpReq.status == 200){
-            areaArray = JSON.parse( myAreaHttpReq.response ).meals
+    myAreaHttpReq.addEventListener('loadend', function () {
+        if (myAreaHttpReq.status == 200) {
+            areaArray = JSON.parse(myAreaHttpReq.response).meals
             displayCountries()
         }
     })
 }
 
-function displayCountries(){
+function displayCountries() {
     document.querySelector('.allMeals').classList.add('d-none')
     document.querySelector('.allIngredients').classList.add('d-none')
-    document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('#info').classList.add('d-none')
     document.querySelector('.mealsCaregory').classList.add('d-none')
-    document.querySelector('.search-inputs').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('.allCategory').classList.add('d-none')
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
+
 
 
 
@@ -165,7 +193,7 @@ function displayCountries(){
 
     var cartona = ``
 
-    for( var i = 0; i < areaArray.length; i++  ){
+    for (var i = 0; i < areaArray.length; i++) {
         cartona += `    <div class="col-lg-3"> 
         <article class="text-center">
             <img src="imgs/world.jpg" alt="kb9" class="w-100 rounded-3">
@@ -176,198 +204,248 @@ function displayCountries(){
 
     document.querySelector('.allCountries').innerHTML = cartona
 
-    $('#side-nav').animate({left: "-280px"}, 800)
+    $('#side-nav').animate({ left: "-280px" }, 800)
     $('.fa-x').css('display', 'none')
     $('.fa-bars').css('display', 'block')
 }
 
-function getIngredients(){
+function getIngredients() {
     let myIngHttpReg = new XMLHttpRequest()
 
-    myIngHttpReg.open('GET','https://www.themealdb.com/api/json/v1/1/list.php?i=list')
+    myIngHttpReg.open('GET', 'https://www.themealdb.com/api/json/v1/1/list.php?i=list')
 
     myIngHttpReg.send()
 
     ingredientsArray = []
 
-    myIngHttpReg.addEventListener('loadend', function(){
-        if(myIngHttpReg.status == 200){
-            ingredientsArray = JSON.parse( myIngHttpReg.response ).meals
+    myIngHttpReg.addEventListener('loadend', function () {
+        if (myIngHttpReg.status == 200) {
+            ingredientsArray = JSON.parse(myIngHttpReg.response).meals
             displayIngredients()
         }
     })
 }
 
-function displayIngredients(){
+function displayIngredients() {
     document.querySelector('.allMeals').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('#info').classList.add('d-none')
     document.querySelector('.mealsCaregory').classList.add('d-none')
-    document.querySelector('.search-inputs').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('.allCategory').classList.add('d-none')
     document.querySelector('.allCountries').classList.add('d-none')
-
-
-
-
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
 
     document.querySelector('.allIngredients').classList.remove('d-none')
 
 
     var cartona = ``
 
-    for( var i = 0; i < 24; i++  ){
-        cartona += `    <div class="col-lg-3"> 
-        <article class="">
+    for (var i = 0; i < 20; i++) {
+        // console.log(ingredientsArray[i].strIngredient);
+        cartona += `<div class="col-lg-3"> 
+        <article onClick="getSpecificIngredients('${ingredientsArray[i].strIngredient}')" class="text-center">
             <img src="imgs/Handaan_Regular.jpg" alt="kb9" class="w-100 rounded-3">
-            <h2 class="text-white"> ${ingredientsArray[i].strIngredient} </h2>
+            <h3 class="text-white"> ${ingredientsArray[i].strIngredient} </h3>
         </article>
     </div>`
     }
 
     document.querySelector('.allIngredients').innerHTML = cartona
 
-    $('#side-nav').animate({left: "-280px"}, 800)
+    $('#side-nav').animate({ left: "-280px" }, 800)
     $('.fa-x').css('display', 'none')
     $('.fa-bars').css('display', 'block')
 }
 
-function displayContactUs(){
-    
+function getSpecificIngredients(namee) {
+    console.log(namee);
+    let myIngHttpReg = new XMLHttpRequest()
+
+    myIngHttpReg.open('GET', `https://www.themealdb.com/api/json/v1/1/filter.php?i=${namee}`)
+
+    myIngHttpReg.send()
+
+    SpecingredientsArray = []
+
+    myIngHttpReg.addEventListener('loadend', function () {
+        if (myIngHttpReg.status == 200) {
+            SpecingredientsArray = JSON.parse(myIngHttpReg.response).meals
+            displaySpecificIngredients()
+            console.log(SpecingredientsArray);
+        }
+    })
+}
+function displaySpecificIngredients() {
+
+    document.querySelector('.allMeals').classList.add('d-none')
+    document.querySelector('.contactUs').classList.add('d-none')
+    document.querySelector('#info').classList.add('d-none')
+    document.querySelector('.mealsCaregory').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
+    document.querySelector('.contactUs').classList.add('d-none')
+    document.querySelector('.allCategory').classList.add('d-none')
+    document.querySelector('.allIngredients').classList.add('d-none')
+    document.querySelector('.allCountries').classList.add('d-none')
+
+    document.querySelector('.SpecificIngredient').classList.remove('d-none')
+
+    var cartona = ``
+
+    for (var i = 0; i < SpecingredientsArray.length; i++) {
+        cartona += `    <div class="col-lg-3"> 
+        <article >
+            <img src='${SpecingredientsArray[i].strMealThumb}' class="w-100 rounded-3">
+            <h2 class="text-white"> ${SpecingredientsArray[i].strMeal} </h2>
+        </article>
+    </div>`
+    }
+
+    document.querySelector('.SpecificIngredient').innerHTML = cartona
+}
+
+function displayContactUs() {
+
     document.querySelector('.allMeals').classList.add('d-none')
     document.querySelector('#info').classList.add('d-none')
     document.querySelector('.mealsCaregory').classList.add('d-none')
-    document.querySelector('.search-inputs').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('.allCategory').classList.add('d-none')
     document.querySelector('.allCountries').classList.add('d-none')
     document.querySelector('.allIngredients').classList.add('d-none')
-
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
 
 
 
     document.querySelector('.contactUs').classList.remove('d-none')
 
-    $('#side-nav').animate({left: "-280px"}, 800)
+    $('#side-nav').animate({ left: "-280px" }, 800)
     $('.fa-x').css('display', 'none')
     $('.fa-bars').css('display', 'block')
 }
 
 
-function displayMealInfo(index){
+function displayMealInfo(index) {
 
 
     document.querySelector('.allMeals').classList.add('d-none')
     document.querySelector('.mealsCaregory').classList.add('d-none')
-    document.querySelector('.search-inputs').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('.allCategory').classList.add('d-none')
     document.querySelector('.allCountries').classList.add('d-none')
-    document.querySelector('.allIngredients').classList.add('d-none')   
+    document.querySelector('.allIngredients').classList.add('d-none')
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
 
     document.querySelector('#info').classList.remove('d-none')
-        
-    
-
-    
-       var imgSorc = mealsArray[index].strMealThumb 
-       console.log(imgSorc)
-    
-       
-            $('#info-img').attr('src', imgSorc)
-            $('.infoName').html( mealsArray[index].strMeal )
-            $('.description').html( mealsArray[index].strInstructions )
-            $('.areaName').html( mealsArray[index].strArea )
-            $('.category').html( mealsArray[index].strCategory )
-
-            $('.rescipies').html(mealsArray[index].strIngredient1)
 
 
-            $('.videoBtn').attr('href', mealsArray[index].strYoutube )
-    
+
+
+    var imgSorc = mealsArray[index].strMealThumb
+    console.log(mealsArray[index]);
+    console.log(imgSorc)
+
+
+    $('#info-img').attr('src', imgSorc)
+    $('.infoName').html(mealsArray[index].strMeal)
+    $('.description').html(mealsArray[index].strInstructions)
+    $('.areaName').html(mealsArray[index].strArea)
+    $('.category').html(mealsArray[index].strCategory)
+
+    $('.rescipies').html(mealsArray[index].strIngredient1)
+
+
+    $('.videoBtn').attr('href', mealsArray[index].strYoutube)
+
 
 }
 
 
 function getMealsBysearch(x) {
+    document.querySelector('.allMeals').classList.remove('d-none')
 
     var mySearchHttpsReq = new XMLHttpRequest()
 
 
-mySearchHttpsReq.open('GET' , `https://www.themealdb.com/api/json/v1/1/search.php?s=${x}`)
+    mySearchHttpsReq.open('GET', `https://www.themealdb.com/api/json/v1/1/search.php?s=${x}`)
 
-mySearchHttpsReq.send()
+    mySearchHttpsReq.send()
 
-mySearchHttpsReq.addEventListener('loadend' , function(){
-    if( mySearchHttpsReq.status == 200 ){
-        mealsArray = JSON.parse(  mySearchHttpsReq.response ).meals
-        console.log( mealsArray )
-        displayMeals()
-    }
-})
-  
+    mySearchHttpsReq.addEventListener('loadend', function () {
+        if (mySearchHttpsReq.status == 200) {
+            mealsArray = JSON.parse(mySearchHttpsReq.response).meals
+            console.log(mealsArray)
+            displayMeals()
+        }
+    })
+
 }
 
-function getMealsBy1stLetter(y){
+function getMealsBy1stLetter(y) {
+
+    document.querySelector('.allMeals').classList.remove('d-none')
+
 
     var regex = / ^[a-z]{1}$ /
 
     console.log(regex)
 
-    console.log( regex.test(y)  )
+    console.log(regex.test(y))
 
-    if ( regex.test(y) == false)  {
+    if (regex.test(y) == false) {
         var mySearchHttpsReq = new XMLHttpRequest()
 
-        mySearchHttpsReq.open('GET' , `https://www.themealdb.com/api/json/v1/1/search.php?f=${y}`)
-        
+        mySearchHttpsReq.open('GET', `https://www.themealdb.com/api/json/v1/1/search.php?f=${y}`)
+
         mySearchHttpsReq.send()
-        
-        mySearchHttpsReq.addEventListener('loadend' , function(){
-            if( mySearchHttpsReq.status == 200 ){
-                mealsArray = JSON.parse(  mySearchHttpsReq.response ).meals
-                console.log( mealsArray )
+
+        mySearchHttpsReq.addEventListener('loadend', function () {
+            if (mySearchHttpsReq.status == 200) {
+                mealsArray = JSON.parse(mySearchHttpsReq.response).meals
+                console.log(mealsArray)
                 displayMeals()
             }
         })
-    } 
-    else{
+    }
+    else {
         console.log('wrong')
     }
 
- 
+
 }
 
 
 
-function getMealsByCategory(z){
+function getMealsByCategory(z) {
     var myCategoryMealsHttp = new XMLHttpRequest()
 
-    myCategoryMealsHttp.open('GET' , `https://www.themealdb.com/api/json/v1/1/filter.php?c=${z}`)
-    
+    myCategoryMealsHttp.open('GET', `https://www.themealdb.com/api/json/v1/1/filter.php?c=${z}`)
+
     myCategoryMealsHttp.send()
-    
-    myCategoryMealsHttp.addEventListener('loadend' , function(){
-        if( myCategoryMealsHttp.status == 200 ){
-            mealsArray = JSON.parse(  myCategoryMealsHttp.response ).meals
-            console.log( mealsArray )
+
+    myCategoryMealsHttp.addEventListener('loadend', function () {
+        if (myCategoryMealsHttp.status == 200) {
+            mealsArray = JSON.parse(myCategoryMealsHttp.response).meals
+            console.log(mealsArray)
             displayCategoryMeals()
         }
     })
 
-    console.log(zby)
 }
 
-function displayCategoryMeals (){
+function displayCategoryMeals() {
 
     document.querySelector('.allMeals').classList.add('d-none')
-    document.querySelector('.search-inputs').classList.add('d-none')
+    // document.querySelector('.search-inputs').classList.add('d-none')
     document.querySelector('.contactUs').classList.add('d-none')
     document.querySelector('.allCategory').classList.add('d-none')
     document.querySelector('.allCountries').classList.add('d-none')
-    document.querySelector('.allIngredients').classList.add('d-none')  
+    document.querySelector('.allIngredients').classList.add('d-none')
     document.querySelector('#info').classList.add('d-none')
+    document.querySelector('.SpecificIngredient').classList.add('d-none')
 
 
     document.querySelector('.mealsCaregory').classList.remove('d-none')
@@ -376,7 +454,7 @@ function displayCategoryMeals (){
 
     var cartona = ``
 
-    for( var i = 0; i < mealsArray.length; i++ ){
+    for (var i = 0; i < mealsArray.length; i++) {
         cartona += `    <div class="col-lg-3"> 
         <article >
             <img src="${mealsArray[i].strMealThumb}" alt="kb9" class="w-100 rounded-3">
@@ -390,4 +468,13 @@ function displayCategoryMeals (){
     }
 
     document.querySelector('.mealsCaregory').innerHTML = cartona
+}
+
+
+
+
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 2000);
 }
